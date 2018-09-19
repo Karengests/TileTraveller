481268d61c6892cf03ad48b84c7cdd1070c4f163
Karen María <karenmaria@Karens-MacBook-Pro.local>
2018-09-19 22:02:19 +0000

    Bjó til breytur og setti það fyrir hvert tile í kassanum

--- tile.traveller1.py
+++ tile.traveller1.py
@@ -1,38 +1,133 @@
-#https://github.com/Karengests/TileTraveller/blob/master/tile.traveller1.py
-x = 1
-y = 1
-tile = (x,y)
+tile = (1,1)
 
-while tile != (3,3)
-    print("You can Travel:", end='')
+direct = '(N)orth.'
+print('You can travel: ',direct)
+
+while tile != (3,1):
+
     if tile == (1,1): 
-        print("You can travel: (N)orth")
+        direction = input('Direction: ')
+        if direction == 'n' or direction == 'N':
+            tile = (1,2)
+            direct = '(N)orth or (E)ast or (S)outh.'
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        else:
+            print('Not a valid direction!')
+            tile = tile
            
     elif tile == (1,2):
-        print("You can travel: (N)orth or (E)ast or (S)outh.")
+        if direction == 'n' or direction =='N':
+            tile = (1,3)
+            direct = '(E)ast or (S)outh.'    
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        elif direction == 'e' or direction == 'E':
+            tile = (2,2)
+            direct = '(S)outh or (W)est.'    
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        elif direction == 's' or direction =='S':
+            tile = (1,1)
+            direct = '(N)orth.'
+            print('You can travel:',direct)
+        else:
+            print('Not a valid direction!')
+            tile = tile
+            direction = input('Direction: ')
 
     elif tile == (1,3):
-        print("You can travel: (S)outh or (E)east")
+        if direction == 's' or direction == 'S':
+            tile = (1,2)
+            direct = '(N)orth or (E)ast or (S)outh.'
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        elif direction == 'e' or direction =='E':
+            tile = (2,3)
+            direct = '(E)ast or (W)est.'    
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        else:
+            print('Not a valid direction!')
+            tile = tile
+            direction = input('Direction: ')
+
 
     elif tile == (2,1):
-        print("You can travel: (N)orth")
+        if direction == 'n' or direction =='N':
+            tile = (2,2)
+            direct = '(S)outh or (W)est.'    
+            print('You can travel:',direct)
+            direction = input('Direction: ')    
+        else:
+            print('Not a valid direction!')
+            tile = tile
+            direction = input('Direction: ')
 
     elif tile == (2,2):
-        print("You can travel: (S)outh or (W)est")
+        if direction == 's' or direction == 'S':
+            tile = (2,1)
+            direct = '(N)orth.'    
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        elif direction == 'w' or direction == 'W':
+            tile = (1,2)  
+            direct = '(N)orth or (E)ast or (S)outh.'
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        else:
+            print('Not a valid direction!')
+            tile = tile
+            direction = input('Direction: ')
 
     elif tile == (2,3):
-        print("You can travel: (W)est or (E)ast")
-
-    elif tile == (3,1):
-        print("Victory!")
+        if direction == 'e' or direction =='E':
+            tile = (3,3)
+            direct = '(S)outh or (W)est.'    
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        elif direction == 'w' or direction =='W':
+            tile = (1,3)
+            direct = '(E)ast our (S)outh.'    
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        else:
+            print('Not a valid direction!')
+            tile = tile
+            direction = input('Direction: ')
 
     elif tile == (3,2):
-        print("You can travel: (N)orth")
+        if direction == 's' or direction == 'S':
+            tile = (3,1) 
+        elif direction == 'n' or direction == 'N':
+            tile = (3,3)
+            direct = '(S)outh or (W)est.'    
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        else:
+            print('Not a valid direction!')
+            tile = tile
+            direction = input('Direction: ')
 
     elif tile == (3,3):
-        print("You can travel: (S)outh or (W)est")
-
-    direction = input(str("Input a direction: "))
+        if direction == 'w' or direction =='W':
+            tile = (2,3)
+            direct = '(E)ast or (W)est.'    
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        elif direction == 's' or direction == 'S':
+            tile = (3,2)
+            direct = '(N)orth or (S)outh.'    
+            print('You can travel:',direct)
+            direction = input('Direction: ')
+        else:
+            print('Not a valid direction!')
+            tile = tile
+            direction = input('Direction: ')
+            
+    if tile == (3,1):
+        print("Victory!")
+    
 
 
 
